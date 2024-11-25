457c31939c5bdbfa0546ac0f85e262790953524e
bub.bl <bub.bl@outlook.com>
2024-11-25 17:29:15 +0100

    Add a new event to be called when a player disconnects

--- Code/Core/Network/INetworkEvents.cs
+++ Code/Core/Network/INetworkEvents.cs
@@ -4,4 +4,5 @@ public interface INetworkEvents : ISceneEvent<INetworkEvents>
 {
 	void OnPlayerConnected( Player player );
 	void OnPlayerJoined( Player player );
+	void OnPlayerDisconnected( Player player, DisconnectedReason reason );
 }
