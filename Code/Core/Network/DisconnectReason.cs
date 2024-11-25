df8b085e87fca66011b2e4846a9448c8b418433e
bub.bl <bub.bl@outlook.com>
2024-11-25 21:39:29 +0100

    Move extensions into the DisconnectReason file

--- Code/Core/Network/DisconnectReason.cs
+++ Code/Core/Network/DisconnectReason.cs
@@ -30,3 +30,24 @@ public enum DisconnectedReason
 	/// </summary>
 	Ping
 }
+
+/// <summary>
+/// Extensions for the <see cref="DisconnectedReason"/> enum
+/// </summary>
+public static class DisconnectReasonExtensions
+{
+	/// <summary>
+	/// Converts the <see cref="DisconnectedReason"/> to a human-readable message
+	/// </summary>
+	/// <param name="disconnectReason">The <see cref="DisconnectedReason"/> to convert</param>
+	/// <returns>The human-readable message</returns>
+	public static string ToMessage( this DisconnectedReason disconnectReason ) => disconnectReason switch
+	{
+		DisconnectedReason.None => "No reason provided for kick.",
+		DisconnectedReason.Disconnected => "Disconnected from the server.",
+		DisconnectedReason.Banned => "Banned from the server.",
+		DisconnectedReason.Afk => "AFK for to long.",
+		DisconnectedReason.Ping => "High ping.",
+		_ => "Unknown reason"
+	};
+}
