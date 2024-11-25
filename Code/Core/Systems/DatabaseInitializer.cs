6e797272cf19b8aca28c17c5a55427909c267fe4
bub.bl <bub.bl@outlook.com>
2024-11-25 15:43:36 +0100

    Add a database initializer component, only used for setup the api connection

---
+++ Code/Core/Systems/DatabaseInitializer.cs
@@ -0,0 +1,35 @@
+﻿using Mongo.Rest;
+
+namespace Orizon.Core.Systems;
+
+public enum DatabaseConnectionKind
+{
+	Localhost,
+	Remote
+}
+
+[Description( "Initializes the database connection" )]
+public sealed class DatabaseInitializer : Component
+{
+	[Property] public DatabaseConnectionKind ConnectionKind { get; private set; }
+
+	[Property, ShowIf( nameof(ConnectionKind), DatabaseConnectionKind.Remote )]
+	public string ConnectionUrl { get; private set; } = "https://localhost:443";
+
+	protected override void OnAwake()
+	{
+		var url = ConnectionKind switch
+		{
+			DatabaseConnectionKind.Localhost => "https://localhost:443",
+			DatabaseConnectionKind.Remote => ConnectionUrl,
+			_ => string.Empty
+		};
+
+		var system = Scene.GetSystem<MongoRestSystem>();
+
+		system.Configure( x =>
+		{
+			x.Url = url;
+		} );
+	}
+}
