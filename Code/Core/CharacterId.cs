7ae21b8f46e5ab911b881578d49f95d65b220a64
bub.bl <bub.bl@outlook.com>
2024-11-25 15:51:20 +0100

    Add docs to CharacterId struct

--- Code/Core/CharacterId.cs
+++ Code/Core/CharacterId.cs
@@ -4,40 +4,59 @@ using Orizon.Core.Json.Converters;
 
 namespace Orizon.Core;
 
+/// <summary>
+/// Represents an Orizon character ID
+/// </summary>
 [JsonConverter( typeof(CharacterIdConverter) )]
 public readonly struct CharacterId
 {
-	private readonly SteamId _steamId;
-	private readonly ushort _characterId;
+	/// <summary>
+	/// The Steam ID of the character owner
+	/// </summary>
+	[JsonIgnore]
+	public SteamId Owner { get; }
 
-	[JsonIgnore] public SteamId Owner => _steamId;
-	[JsonIgnore] public ushort Id => _characterId;
+	/// <summary>
+	/// The ID of the character
+	/// </summary>
+	[JsonIgnore]
+	public ushort Id { get; }
 
-	public CharacterId( SteamId steamId, ushort characterId ) => (_steamId, _characterId) = (steamId, characterId);
+	/// <summary>
+	/// Initializes a new <see cref="CharacterId"/>
+	/// </summary>
+	/// <param name="steamId">The Steam ID of the character owner</param>
+	/// <param name="characterId">The ID of the character</param>
+	public CharacterId( SteamId steamId, ushort characterId ) => (Owner, Id) = (steamId, characterId);
 
-	private CharacterId( string characterId )
+	/// <summary>
+	/// Parses a string into a new <see cref="CharacterId"/>
+	/// </summary>
+	/// <param name="characterId">The string to parse</param>
+	/// <returns>The parsed <see cref="CharacterId"/></returns>
+	/// <exception cref="ArgumentException">If the string is not a valid <see cref="CharacterId"/></exception>
+	public static CharacterId Parse( string characterId )
 	{
 		var parts = characterId.Split( ':' );
 
 		if ( parts.Length != 2 )
-			throw new ArgumentException( $"Invalid {nameof(CharacterId)} format." );
+			throw new ArgumentException( $"Invalid {nameof(CharacterId)} format.", nameof(characterId) );
 
 		var steamId = SteamId.Parse( parts[0] );
-		_steamId = steamId;
+		var characterIdParsed = ushort.TryParse( parts[1], out var characterIdValue );
 
-		if ( steamId is 0 )
-			throw new ArgumentException( "Invalid Steam ID." );
+		if ( !characterIdParsed )
+			throw new ArgumentException( $"Invalid {nameof(CharacterId)} format.", nameof(characterId) );
 
-		var parsed = ushort.TryParse( parts[1], out _characterId );
-
-		if ( !parsed )
-			throw new ArgumentException( $"Invalid {nameof(CharacterId)}." );
+		return new CharacterId( steamId, characterIdValue );
 	}
 
-	public static CharacterId Parse( string characterId ) => new(characterId);
-
-	public override string ToString() => $"{_steamId}:{_characterId}";
+	/// <summary>
+	/// Converts the <see cref="CharacterId"/> to a string
+	/// </summary>
+	/// <returns>The string representation of the <see cref="CharacterId"/></returns>
+	public override string ToString() => $"{Owner}:{Id}";
 
 	public static implicit operator string( CharacterId characterId ) => characterId.ToString();
-	public static implicit operator CharacterId( string characterId ) => new(characterId);
+	public static implicit operator CharacterId( string characterId ) => Parse( characterId );
 }
