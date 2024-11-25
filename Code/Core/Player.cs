c357d6d0e51f212f156a4cfc1e291ee975fd771f
bub.bl <bub.bl@outlook.com>
2024-11-25 17:15:13 +0100

    Add player component that store and own all player related data

---
+++ Code/Core/Player.cs
@@ -0,0 +1,30 @@
+﻿namespace Orizon.Core;
+
+public sealed class Player : Component
+{
+	[HostSync, Property] public SteamId SteamId { get; set; }
+	[HostSync, Property] public string SteamName { get; set; } = null!;
+
+	public static Player Local { get; private set; } = null!;
+	public Connection? Connection => Network.Owner;
+	public bool IsConnected => Connection is not null && (Connection.IsActive || Connection.IsHost);
+	public bool IsLocalPlayer => !IsProxy && Connection == Connection.Local;
+
+	internal void HostInit()
+	{
+		if ( Connection is null )
+		{
+			Log.Error( "Failed to get player connection" );
+			return;
+		}
+
+		SteamId = Connection.SteamId;
+		SteamName = Connection.DisplayName;
+	}
+
+	[Authority]
+	internal void ClientInit()
+	{
+		Local = this;
+	}
+}
