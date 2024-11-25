13af85c6feb743b1184ec76f569e494d5a73f85f
bub.bl <bub.bl@outlook.com>
2024-11-25 15:54:10 +0100

    Add docs for Gender enum

--- Code/Core/Gender.cs
+++ Code/Core/Gender.cs
@@ -1,7 +1,16 @@
 ﻿namespace Orizon.Core;
 
+/// <summary>
+/// Represents the gender of a character
+/// </summary>
 public enum Gender
 {
+	/// <summary>
+	/// Represents the male gender
+	/// </summary>
 	Male,
+	/// <summary>
+	/// Represents the female gender
+	/// </summary>
 	Female
 }
