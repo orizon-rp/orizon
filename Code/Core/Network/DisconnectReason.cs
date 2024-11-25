fd84f369e87ef203e8db4ab4e4a5f70f5a4300e8
bub.bl <bub.bl@outlook.com>
2024-11-25 17:27:47 +0100

    Add disconnected reason enum

---
+++ Code/Core/Network/DisconnectReason.cs
@@ -0,0 +1,32 @@
+﻿namespace Orizon.Core.Network;
+
+/// <summary>
+/// Represents the reason for a disconnection from the server
+/// </summary>
+public enum DisconnectedReason
+{
+	/// <summary>
+	/// No reason provided
+	/// </summary>
+	None,
+
+	/// <summary>
+	/// Disconnected from the server
+	/// </summary>
+	Disconnected,
+
+	/// <summary>
+	/// Banned from the server
+	/// </summary>
+	Banned,
+
+	/// <summary>
+	/// Kicked due to inactivity
+	/// </summary>
+	Afk,
+	
+	/// <summary>
+	/// Kicked due to a high ping
+	/// </summary>
+	Ping
+}
