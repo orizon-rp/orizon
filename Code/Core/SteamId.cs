72d64fd98ec1e2a30da90c5d817bf6d0dff73b9b
bub.bl <bub.bl@outlook.com>
2024-11-25 15:50:08 +0100

    Add docs to SteamId struct

--- Code/Core/SteamId.cs
+++ Code/Core/SteamId.cs
@@ -4,44 +4,83 @@ using Orizon.Core.Json.Converters;
 
 namespace Orizon.Core;
 
+/// <summary>
+/// Represents a SteamID
+/// </summary>
 [JsonConverter( typeof(SteamIdConverter) )]
 public readonly struct SteamId : IEquatable<SteamId>, IEquatable<ulong>, IComparable, IComparable<SteamId>,
 	IComparable<ulong>
 {
 	private readonly ulong _value;
 
+	/// <summary>
+	/// Gets the SteamID of the local user
+	/// </summary>
 	public static SteamId Local => Game.SteamId;
 
+	/// <summary>
+	/// Initializes a new SteamID from an ulong
+	/// </summary>
+	/// <param name="value"></param>
 	private SteamId( ulong value )
 	{
 		_value = value;
 	}
 
+	/// <summary>
+	/// Checks if two SteamIDs are equal
+	/// </summary>
+	/// <param name="other"></param>
+	/// <returns></returns>
 	public bool Equals( SteamId other )
 	{
 		return _value == other._value;
 	}
 
+	/// <summary>
+	/// Checks if a SteamID is equal to an ulong
+	/// </summary>
+	/// <param name="other"></param>
+	/// <returns></returns>
 	public bool Equals( ulong other )
 	{
 		return _value == other;
 	}
 
+	/// <summary>
+	/// Compares a SteamID to an ulong
+	/// </summary>
+	/// <param name="other"></param>
+	/// <returns></returns>
 	public int CompareTo( ulong other )
 	{
 		return _value.CompareTo( other );
 	}
 
+	/// <summary>
+	/// Checks if a SteamID is equal to an object
+	/// </summary>
+	/// <param name="obj"></param>
+	/// <returns></returns>
 	public override bool Equals( object? obj )
 	{
 		return obj is SteamId other && Equals( other );
 	}
 
+	/// <summary>
+	/// Gets the hash code for the SteamID
+	/// </summary>
+	/// <returns></returns>
 	public override int GetHashCode()
 	{
 		return _value.GetHashCode();
 	}
 
+	/// <summary>
+	/// Compares a SteamID to an object
+	/// </summary>
+	/// <param name="obj"></param>
+	/// <returns></returns>
 	public int CompareTo( object? obj )
 	{
 		if ( obj is SteamId other )
@@ -50,16 +89,30 @@ public readonly struct SteamId : IEquatable<SteamId>, IEquatable<ulong>, ICompar
 		return _value.CompareTo( obj );
 	}
 
+	/// <summary>
+	/// Compares a SteamID to another SteamID
+	/// </summary>
+	/// <param name="other"></param>
+	/// <returns></returns>
 	public int CompareTo( SteamId other )
 	{
 		return _value.CompareTo( other._value );
 	}
-	
+
+	/// <summary>
+	/// Parses a string into a SteamID
+	/// </summary>
+	/// <param name="value"></param>
+	/// <returns></returns>
 	public static ulong Parse( string value )
 	{
 		return ulong.TryParse( value, out var steamId ) ? steamId : 0;
 	}
-	
+
+	/// <summary>
+	/// Converts the SteamID to a string
+	/// </summary>
+	/// <returns></returns>
 	public override string ToString() => _value.ToString();
 
 	public static implicit operator ulong( SteamId id ) => id._value;
