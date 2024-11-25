eeec4a4fb039d0091ef447e2a2e64ccfa81a8cc0
bub.bl <bub.bl@outlook.com>
2024-11-25 15:36:18 +0100

    Prepare the game manager component

---
+++ Code/Core/Systems/GameManager.cs
@@ -0,0 +1,10 @@
+﻿namespace Orizon.Core.Systems;
+
+public sealed class GameManager : Component, Component.INetworkListener
+{
+	void INetworkListener.OnActive( Connection channel )
+	{
+		// TODO - Create a PlayerData for the new player and insert it into the database
+		// (Only if the player is not already in the database)
+	}
+}
