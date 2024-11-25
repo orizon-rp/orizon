49780a783cdffa78a94843ff07ab837a57b58a02
bub.bl <bub.bl@outlook.com>
2024-11-25 16:47:23 +0100

    Add vitals data to character data

--- Code/Core/Models/CharacterData.cs
+++ Code/Core/Models/CharacterData.cs
@@ -13,5 +13,14 @@ public record CharacterData
 	public string LastName { get; init; } = null!;
 	public DateTime DateOfBirth { get; init; }
 	public Gender Gender { get; init; }
+	public VitalsData Vitals { get; init; } = null!;
 	public DateTime CreatedAt { get; init; }
+
+	public record VitalsData
+	{
+		public float Health { get; init; }
+		public float Stamina { get; init; }
+		public float Hunger { get; init; }
+		public float Thirst { get; init; }
+	}
 }
