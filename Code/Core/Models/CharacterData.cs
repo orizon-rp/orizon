89357e0e0a305b8fe752932641b60a24d8da2743
bub.bl <bub.bl@outlook.com>
2024-11-25 15:30:40 +0100

    Create a basic character record to store some default information about the character

---
+++ Code/Core/Models/CharacterData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using Mongo.Rest;
+
+namespace Orizon.Core.Models;
+
+[MongoCollection( "characters" )]
+public record CharacterData
+{
+	[JsonPropertyName( "_id" )] public CharacterId Id { get; init; } = null!;
+
+	public string FirstName { get; init; } = null!;
+	public string LastName { get; init; } = null!;
+	public DateTime DateOfBirth { get; init; }
+	public Gender Gender { get; init; }
+	public DateTime CreatedAt { get; init; }
+}
