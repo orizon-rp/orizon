94cca90a8e6e10e81c2b9da4bc4dc18d44fd247f
bub.bl <bub.bl@outlook.com>
2024-11-25 15:47:46 +0100

    Add docs to database initializer

--- Code/Core/Systems/DatabaseInitializer.cs
+++ Code/Core/Systems/DatabaseInitializer.cs
@@ -2,17 +2,47 @@
 
 namespace Orizon.Core.Systems;
 
+/// <summary>
+/// Defines how the database connection should be initialized.
+/// </summary>
+/// <remarks>
+/// The database connection is initialized based on the value of this enum.
+/// </remarks>
 public enum DatabaseConnectionKind
 {
+	/// <summary>
+	/// Connect to a local instance of the database.
+	/// </summary>
+	/// <remarks>
+	/// This is the default connection kind.
+	/// </remarks>
 	Localhost,
+
+	/// <summary>
+	/// Connect to a remote instance of the database, using the <see cref="DatabaseInitializer.ConnectionUrl"/> property.
+	/// </summary>
+	/// <remarks>
+	/// The <see cref="DatabaseInitializer.ConnectionUrl"/> property must be set if this connection kind is used.
+	/// </remarks>
 	Remote
 }
 
 [Description( "Initializes the database connection" )]
 public sealed class DatabaseInitializer : Component
 {
+	/// <summary>
+	/// Defines how the database connection should be initialized.
+	/// <list type="bullet">
+	/// <item><see cref="DatabaseConnectionKind.Localhost"/>: Connect to a local instance of the database.</item>
+	/// <item><see cref="DatabaseConnectionKind.Remote"/>: Connect to a remote instance of the database, using the <see cref="ConnectionUrl"/> property.</item>
+	/// </list>
+	/// </summary>
 	[Property] public DatabaseConnectionKind ConnectionKind { get; private set; }
 
+	/// <summary>
+	/// The URL of the remote database connection.
+	/// Only applicable if <see cref="ConnectionKind"/> is set to <see cref="DatabaseConnectionKind.Remote"/>.
+	/// </summary>
 	[Property, ShowIf( nameof(ConnectionKind), DatabaseConnectionKind.Remote )]
 	public string ConnectionUrl { get; private set; } = "https://localhost:443";
 
